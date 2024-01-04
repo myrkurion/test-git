6506e06d404cb24e8b79303a6fe3513515be7ef4
myrkurion <nikhromov.m@gmail.com>
2024-01-04 05:43:29 +0300

    Removed one of the messages

--- src/main.py
+++ src/main.py
@@ -2,4 +2,5 @@ if __name__ == "__main__":
     print("Let's go")
     #Removed change, make this change a major version
     #print("Another change")
-    print("Change, introduced in github")
+    #Also removed this. Let's diverge the history 
+    #print("Change, introduced in github")
