4920fec3976b6c08bb63ba941d4a0dd5afa58a08
myrkurion <nikhromov.m@gmail.com>
2024-01-04 05:54:58 +0300

    Merging conflict introduced

--- src/main.py
+++ src/main.py
@@ -2,4 +2,6 @@ if __name__ == "__main__":
     print("Let's go")
     #Removed change, make this change a major version
     #print("Another change")
+    
+    #Let's create a merge conflict. 
     print("Change, introduced in github")
